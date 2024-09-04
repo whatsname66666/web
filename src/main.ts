31dbd80287ffad1dfaf4d7bd9d3fa505cd345508
whatsname66666 <1647087433@qq.com>
2024-09-04 10:08:33 +0800

    打印日志
    log

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import globalComponents from '@/components'
 const app = createApp(App)
 // 国际化
+console.log(app)
 app.use(ElementPlus, {
     locale: zhCn,
 })
