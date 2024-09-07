74b98da8208d44b87dd9fba933ec42b387daca4d
whatsname66666 <1647087433@qq.com>
2024-09-07 11:12:17 +0800

    modifly
    css变量加color-picker组件实现自定义主题

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import globalComponents from '@/components'
 const app = createApp(App)
 // 国际化
-console.log(app)
 app.use(ElementPlus, {
     locale: zhCn,
 })
