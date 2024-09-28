510bbed91f5320aa2f4499469f27715a6eb8b514
whatsname66666 <1647087433@qq.com>
2024-09-28 17:57:29 +0800

    modified
    双色球选中规则

--- src/page/home/es6/ES6.vue
+++ src/page/home/es6/ES6.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts" name="es6">
+//在js里面取对象的属性
+function fn(obj,key){
+  return obj[key]
+}
+//没有keyof的ts类型标准
+function fn1(obj:object,key:string):any{
+  return obj[key]
+}
+//上面的类型声明有两个问题，一是无法表示参数key与参数obj之间的关系，二是返回值类型只能写成any
 
+function fn2<O,K extends keyof O>(obj:O,key:K):O[K]{
+  return obj[key]
+}
+//K extends keyof O表示K是O的一个属性名，传入其他字符串会报错
+//返回值类型O[K]就表示K这个属性值的类型。
 </script>
 
 <template>
--- src/page/home/js/Promise.vue
+++ src/page/home/js/Promise.vue
@@ -1,13 +1,83 @@
 <script setup lang="ts" name="promise">
-
+import { ref } from 'vue';
+/* 
+彩票规则：
+    6个红色球，1个蓝色球。
+    红色球从数字1~33之间任意一个球，且不重复。
+    蓝色球从数字1~16之间任意一个球。
+中奖规则：
+    一等奖：所有选中的6个红色号码全都中且1个蓝色号码选中，红色号码选中不区分顺序
+    二等奖：所有选中的6个红色号码全都中
+    三等奖：所有选中的5个红色号码全都中外加一个蓝色球 
+    ...   
+*/
+const RED_BALL = 32;
+const BULE_BALL = 15;
+const RandomNumber = (num:number):number =>  {
+    return Math.ceil(Math.random() * num)
+}
+let ballNumber = ref<Array<number>>([1, 2, 3, 4, 5, 6, 7]);
+const RefshNum = (data:Array<number>) => {
+    if(data.length < 7) {
+        let num = RandomNumber(RED_BALL)
+        if(data.includes(num)){
+            RefshNum(data)
+        }else{
+            data.push(num)
+            if(data.length === 6){
+                data.push(RandomNumber(BULE_BALL))
+            }
+            RefshNum(data)
+        }        
+    }
+    return data
+}
+const Star = () => { 
+    ballNumber.value = RefshNum([])
+}
+const End = () => {}
 </script>
 
 <template>
-    <h3>
-        <span class="TitlelinearGradient">Promise</span>
-    </h3>
+  <h3>
+    <p class="TitlelinearGradient">双色球模拟</p>
+  </h3>
+  <div>
+    <ul class="lottery_ticket">
+      <li v-for="i in ballNumber">
+        {{ i }}
+      </li>
+    </ul>
+    <div class="box">
+      <el-button @click="Star" type="primary">开始选号</el-button>
+      <el-button @click="End" type="primary">结束选号</el-button>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="less">
-
+.lottery_ticket {
+  margin: 100px 0;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-evenly;
+  li {
+    flex: 1;
+    height: 100px;
+    line-height: 100px;
+    border-radius: 5px;
+    border: 2px solid #e30b0b;
+    border-right: none;
+    text-align: center;
+    font-size: 20px;
+    text-align: center;
+  }
+  li:last-child {
+    border: 2px solid #0d41dc ;
+  }
+}
+.box {
+  display: flex;
+  justify-content: space-around;
+}
 </style>
